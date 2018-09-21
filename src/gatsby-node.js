da227a1765de774800d5c1b47cad7b4fb40b961b
Rob Walker <rob@papertrail.io>
2018-09-21 07:29:18 +0100

    Initial commit

---
+++ src/gatsby-node.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const regexEscape = require("regex-escape");
+
+exports.onCreateWebpackConfig = ({ actions, loaders }, { modules = [] }) => {
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: modulePath =>
+            /node_modules/.test(modulePath) &&
+            // whitelist specific es6 module
+            !new RegExp(
+              `node_modules\/(${modules.map(regexEscape).join("|")})\/`
+            ).test(modulePath),
+          use: loaders.js()
+        }
+      ]
+    }
+  });
+};
